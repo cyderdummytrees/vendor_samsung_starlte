43d221fc40f19d87837e97e9c60db1da8e8489a8
Phạm Nguyễn Gia Khiêm <pngkhiem21@vp.fitus.edu.vn>
2022-06-18 20:43:08 +0700

    starlte: Remove OMX
    This has been commonized.
    
    Signed-off-by: Phạm Nguyễn Gia Khiêm <pngkhiem21@vp.fitus.edu.vn>

--- starlte-vendor.mk
+++ starlte-vendor.mk
@@ -54,9 +54,6 @@ PRODUCT_PACKAGES += \
     libexynoscamera3 \
     libhifistill \
     libopenvx \
-    libOMX.Exynos.AVC.Encoder \
-    libOMX.Exynos.HEVC.Encoder \
-    libOMX.Exynos.HEVC.WFD.Encoder \
     sensors.sensorhub \
     libaudioeffectoffload \
     audio.primary.exynos9810 \
